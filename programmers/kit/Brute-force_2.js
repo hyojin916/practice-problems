7a7de011e6ae9fb0bbe4f7200a69006cf00b954d
hyojin <gywlsh274@gmail.com>
2023-06-02 18:45:02 +0900

    완전탐색, 탐욕법 진행 증

--- programmers/kit/Brute-force_2.js
+++ programmers/kit/Brute-force_2.js
@@ -1,55 +1,50 @@
-// TODO: 틀림
-const alphabetMap = new Map();
-const alphabetArr = Array.from({ length: 26 }, (v, i) =>
-  String.fromCharCode(i + 65)
-);
+// TODO 틀림
 
-alphabetMap.set('A', 0);
-for (let i = 1; i < 13; i++) {
-  alphabetMap.set(alphabetArr[i], i);
-}
-let count = 0;
-for (let i = 25; i > 12; i--) {
-  count = count + 1;
-  alphabetMap.set(alphabetArr[i], count);
-}
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42840
+ *  */
 
-// 1. 상하 이동 줄이기
-// name을 순회하며 알파벳을 찾는다.
-// 1-1. 알파벳 M 까지 검색할 때는 up, N 까지 검색할 때는 down
+// * 1, 2, 3 번을 모두 10000개 비교하는 것은너무한데?
+// 맞춘 문제의 갯수를 구해야 한다.
+// for문을 3개 돌린다. 3n
+const one = [1, 2, 3, 4, 5];
+const two = [2, 1, 2, 3, 2, 4, 2, 5];
+const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
-function solution(name) {
-  let counter = 0;
-  // 1. name String -> Arr
-  const nameArr = name.split('');
+function solution(answers) {
+  const oneNum = Math.floor(one.length / answers.length);
+  const twoNum = Math.floor(two.length / answers.length);
+  const threeNum = Math.floor(three.length / answers.length);
 
-  // 2. 좌우 이동 줄이기
-  // 2-1. 마지막 글자가 A 일때 Next 하지않는다.
-  // 2-2. 첫 번째, 마지막 글자만 A 일때 거꾸로 간다.
-  let ACounter = nameArr.filter((alpabet) => alpabet === 'A');
-  if (nameArr.length === 1) return alphabetMap.get(nameArr[0]);
+  const oneRemain = answers.length % one.length;
+  const twoRemain = answers.length % two.length;
+  const threeRemain = answers.length % three.length;
 
-  if (
-    ACounter.length === nameArr.length - 2 &&
-    nameArr[0] !== 'A' &&
-    nameArr[nameArr.length - 1] !== 'A'
-  ) {
-    console.log('으으아');
-    return (
-      alphabetMap.get(nameArr[0]) +
-      alphabetMap.get(nameArr[nameArr.length - 1]) +
-      1
-    );
-  }
-
-  for (let i = 0; i < nameArr.length; i++) {
-    if (nameArr[i] !== 'A') {
-      counter = counter + alphabetMap.get(nameArr[i]);
-    }
-    if (nameArr.length - 1 !== i) {
-      counter++;
+  let oneRes = 0;
+  for (let i = 0; i < oneNum; i++) {
+    for (let j = 0; j < one.length; j++) {
+      if (oneNum[i] === one[j]) {
+        oneRes = oneRes + 1;
+      }
     }
   }
 
-  return counter;
+  let twoRes = 0;
+  for (let i = 0; i < twoNum; i++) {
+    for (let j = 0; j < two.length; j++) {
+      if (twoNum[i] === two[j]) {
+        twoRes = twoRes + 1;
+      }
+    }
+  }
+
+  let threeRes = 0;
+  for (let i = 0; i < threeNum; i++) {
+    for (let j = 0; j < three.length; j++) {
+      if (threeNum[i] === three[j]) {
+        threeRes = threeRes + 1;
+      }
+    }
+  }
+  console.log(oneRes, twoRes, threeRes);
 }
