0779ca16da7e99fb18cf9de20a844724ddf91e7d
hyojin <gywlsh274@gmail.com>
2023-09-08 22:30:12 +0900

    Data Structures-Binary Search Tree & Binary Tree - bfs, dfs

---
+++ Data Structures/Tree/binary_search_tree.js
@@ -0,0 +1,124 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value === current.value) return undefined;
+        if (value < current.value) {
+          // left
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          // right
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return current; // return true;
+      }
+    }
+    return false;
+  }
+
+  // * ⭐️ 이진탐색트리가 아니라 그냥 이진트리면 된다. -  BFS & DFS
+  // * 트리 > 이진트리 > 이진 탐색 트리
+  BFS() {
+    // 이건 이진트리면 된다.
+    let node = this.root;
+    let data = [];
+    let queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  DFSPreOrder() {
+    let data = []; // 마지막에 출력 될 변수
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  DFSPostOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      node.left && traverse(node.left);
+      data.push(node.value);
+      node.right && traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+}
+
+//     10
+//   6     15
+// 3  8       20
+
+let tree = new BinarySearchTree();
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.DFSPreOrder(); // [10, 6, 3, 8, 15, 20]
+tree.DFSPostOrder(); // [3, 8, 6, 20, 15, 10]
+tree.DFSInOrder(); // [3, 6, 8, 10, 15, 20]
