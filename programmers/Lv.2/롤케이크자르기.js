9e8fbd0f3f9c7dabcee19bc70d83f54afd0c618d
hyojin <gywlsh274@gmail.com>
2023-10-10 15:22:53 +0900

    lv.2 롤케이크 자르기 다시 풀기 - Map

--- programmers/Lv.2/롤케이크자르기.js
+++ programmers/Lv.2/롤케이크자르기.js
@@ -1,5 +1,32 @@
-// * 조건을 보면 시간초과가 날수밖에없음. Map으로 다시 풀기
+// https://school.programmers.co.kr/learn/courses/30/lessons/132265
+// * 처음 풀었던 방법 (Slice, Set)은 for 문을 돌면서 계속 재할당해주게 되어 메모리 초과.
+// * Map으로 풀며 형의 토핑을 동생에게 주는 방식으로 해결
 
+function solution(topping) {
+  let answer = 0;
+
+  let me = new Map();
+  let bro = new Map();
+
+  for (elem of topping) {
+    me.has(elem) ? me.set(elem, me.get(elem) + 1) : me.set(elem, 1);
+  }
+
+  for (let i = 0; i < topping.length; i++) {
+    const item = topping[i];
+    me.set(item, me.get(item) - 1);
+    bro.has(item) ? bro.set(item, me.get(item) + 1) : bro.set(item, 1);
+
+    if (me.get(item) === 0) me.delete(item);
+    if (me.size === bro.size) answer++;
+  }
+  return answer;
+}
+
+// * 조건(제한사항)을 보면 시간초과가 날수밖에없음. Map으로 다시 풀기
+// 제한사항
+// 1 ≤ topping의 길이 ≤ 1,000,000
+// 1 ≤ topping의 원소 ≤ 10,000
 function solution(topping) {
   let answer = 0;
   let a = [];
